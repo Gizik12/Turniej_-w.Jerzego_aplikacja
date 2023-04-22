fab65884e230e9bffe37df3f49eab5ea38ab5616
Mateusz Gizicki <76407981+Gizik12@users.noreply.github.com>
2023-04-22 12:39:44 +0200

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,10 +1,10 @@
 function start()
 {
-    let tab = {"Aenigmatici lupi":14,"Ametyst":0,"Andromeda":16,"Białe wilki":0,"Bryza":0,"Cefeusz":0,"Delfiny":14,"Dragoni":0,"Dziubki":0,
+    let tab = {"Aenigmatici lupi":26,"Ametyst":0,"Andromeda":23,"Białe wilki":0,"Bryza":0,"Cefeusz":0,"Delfiny":24,"Dragoni":0,"Dziubki":0,
                "Feniksy":35,"Forti lupi":31,"Halit":32,"Ilex":0,"Imbricaria":0,"Jastrzębie":0,"Jutrzenka":19,"Kaczki":0,"Kasjopeja":11,"Kotlina":0,
-               "Lisowczycy":0,"Lisy":20,"Lupi citi":0,"Macranthera":32,"Orion":26,"Palustris":0,"Pancerni":18,"Pectinata":52,"Petryhorcy":0,
-               "Pingwiny":28,"Prinus":20,"Promienie":0,"Przełęcz":0,"Płomienie":0,"Rajtarzy":0,"Rapidi lupi":8,"Rarogi":20,"Salix":20,"Sambucus":39,"Sępy":4,
-               "Silva herbis":0,"Sorbus":37,"Suber":10,"Szafir":0,"Uszatki":5,"Wzgórze":29,"Zefir":16,"Żubry":0};
+               "Lisowczycy":0,"Lisy":20,"Lupi citi":0,"Macranthera":32,"Orion":26,"Palustris":16,"Pancerni":34,"Pectinata":54,"Petryhorcy":0,
+               "Pingwiny":48,"Prinus":20,"Promienie":0,"Przełęcz":0,"Płomienie":0,"Rajtarzy":0,"Rapidi lupi":8,"Rarogi":20,"Salix":20,"Sambucus":39,"Sępy":4,
+               "Silva herbis":0,"Sorbus":57,"Suber":10,"Szafir":0,"Uszatki":5,"Wzgórze":49,"Zefir":16,"Żubry":4};
     let sortedEntries = Object.entries(tab).sort((a, b) => b[1] - a[1]);
     let sortedObj = {};
     for (let entry of sortedEntries) 
